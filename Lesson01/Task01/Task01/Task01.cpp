dbb4d116a4becfd247996df61ca0c843711345d0
pcsln <pcsln@bk.ru>
2023-06-04 14:37:19 +0400

    Fixed Lesson 1 Task 1

---
+++ Lesson01/Task01/Task01/Task01.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int N = 0;
+    int M = 0;
+
+    std::ifstream file_in("in.txt");
+
+    if (!file_in.is_open()) {
+        std::cout << "Не получилось открыть файл in.txt\n";
+        return 1;
+    }
+
+    file_in >> N;
+    int* array_N = new int[N];
+    for (int i = 0; i < N; ++i) {
+        file_in >> array_N[i];
+    }
+
+    file_in >> M;
+    int* array_M = new int[M];
+    for (int i = 0; i < M; ++i) {
+        file_in >> array_M[i];
+    }
+
+    file_in.close();
+
+    int temp = array_N[0];
+    for (int i = 0; i < N - 1; ++i) {
+        array_N[i] = array_N[i + 1];
+    }
+    array_N[N - 1] = temp;
+
+    temp = array_M[M - 1];
+    for (int i = M - 1; i > 0; --i) {
+        array_M[i] = array_M[i - 1];
+    }
+    array_M[0] = temp;
+
+    std::ofstream file_out("out.txt");
+
+    if (!file_out.is_open()) {
+        std::cout << "Не получилось открыть файл out.txt\n";
+        return 1;
+    }
+
+    file_out << M << "\n";
+    for (int i = 0; i <= M - 1; ++i) {
+        file_out << array_M[i] << " ";
+    }
+    file_out << "\n";
+
+    file_out << N << "\n";
+    for (int i = 0; i <= N - 1; ++i) {
+        file_out << array_N[i] << " ";
+    }
+    file_out << "\n";
+
+    file_out.close();
+    delete[] array_N;
+    delete[] array_M;
+}
---
+++ Lesson02/Task01/months/months.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+
+enum class months {
+    Jan = 1,
+    Feb,
+    Mar,
+    Apr,
+    May,
+    Jun,
+    Jul,
+    Aug,
+    Sep,
+    Oct,
+    Nov,
+    Dec
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int choice = 13;
+
+    while (choice != 0) {
+
+        std::cout << "Введите номер месяца:" << std::endl;
+        std::cin >> choice;
+
+        if (choice == 0) { break; };
+
+        months month = static_cast<months>(choice);
+
+        switch (month)
+        {
+        case (months::Jan):
+            std::cout << "Январь - году начало, зиме серёдка." << std::endl;
+            break;
+        case (months::Feb):
+            std::cout << "Февраль - зима с весной встречается впервой." << std::endl;
+            break;
+        case (months::Mar):
+            std::cout << "Март - месяц любит куролесить." << std::endl;
+            break;
+        case (months::Apr):
+            std::cout << "Апрель - воду подбирает, цветы раскрывает." << std::endl;
+            break;
+        case (months::May):
+            std::cout << "Май - леса наряжает, лето в гости ожидает." << std::endl;
+            break;
+        case (months::Jun):
+            std::cout << "Июнь - солнце высоко, с утра до вечера далеко." << std::endl;
+            break;
+        case (months::Jul):
+            std::cout << "Июль - перелом лета, месяц красного цвета." << std::endl;
+            break;
+        case (months::Aug):
+            std::cout << "Август - солнце греет, а вода холодеет." << std::endl;
+            break;
+        case (months::Sep):
+            std::cout << "Сентябрь - красно лето провожает, осень златую встречает." << std::endl;
+            break;
+        case (months::Oct):
+            std::cout << "Октябрь - до обеда осень, после обеда зима." << std::endl;
+            break;
+        case (months::Nov):
+            std::cout << "Ноябрь - месяц перволедья, санного первопутка." << std::endl;
+            break;
+        case (months::Dec):
+            std::cout << "Декабрь - год кончает, зиму начинает." << std::endl;
+            break;
+        default:
+            std::cout << "Неправильное число." << std::endl;
+            break;
+        }
+    }
+
+    std::cout << "До свидания!";
+}
