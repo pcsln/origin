072ed7ca9b4ca4cb38ccf84d129b4118cddb8b6d
pcsln <pcsln@bk.ru>
2023-06-09 15:51:53 +0400

    Lesson 1 and 2

---
+++ Lesson02/Accounts/Accounts.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+struct accounts {
+    long int bank_account;
+    std::string name;
+    double balance;
+};
+
+void create_account(accounts* account) {
+    std::cout << "Введите номер счета: ";
+    std::cin >> account->bank_account;
+    std::cout << "Введите имя владельца: ";
+    std::cin >> account->name;
+    std::cout << "Введите баланс: ";
+    std::cin >> account->balance;
+    std::cout << std::endl;
+}
+
+void change_account(accounts* account) {
+    std::cout << "Введите новый баланс: ";
+    std::cin >> account->balance;
+    std::cout << std::endl;
+}
+
+void print_account(accounts* account) {
+    std::cout << "Ваш счёт: " << account->name << ", " << account->bank_account << ", " << account->balance << std::endl;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    accounts account = { 0, "", 0 };
+
+    create_account(&account);
+
+    change_account(&account);
+
+    print_account(&account);
+
+}
---
+++ Lesson02/Output struct/Output struct.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+struct address {
+    std::string city;
+    std::string street;
+    int house;
+    int appartment;
+    int index;
+};
+
+void output_structure(address office) {
+     std::cout << "Город: " << office.city << std::endl;
+     std::cout << "Улица: " << office.street << std::endl;
+     std::cout << "Номер дома: " << office.house << std::endl;
+     std::cout << "Номер квартиры: " << office.appartment << std::endl;
+     std::cout << "Индекс: " << office.index << std::endl;
+     std::cout << std::endl;
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int N = 4;
+
+    address* offices = new address[N];
+
+    offices[0] = { "Ульяновск", "Карла Маркса", 33, 29, 432700 };
+    offices[1] = { "Ульяновск", "Строителей", 12, 25, 432013 };
+    offices[2] = { "Новоульяновск", "Заводская", 1, 4, 433234 };
+    offices[3] = { "Димитровград", "Циолковского", 54, 544, 435019 };
+
+    for (int i = 0; i < N; ++i) {
+        output_structure(offices[i]);
+    }
+    
+    delete[] offices;
+}
