c5088e8e03255d521ff9663e673a071721709be2
pcsln <pcsln@bk.ru>
2023-06-15 15:11:16 +0400

    Added Lesson05

---
+++ Lesson05/Figures. Sides and angles/Figures. Sides and angles.cpp
@@ -0,0 +1,226 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class Figure {
+protected:
+    int sides_count;
+    std::string name;
+public:
+    Figure() {
+        this->sides_count = 0;
+    }
+    int get_sides_count() {
+        return sides_count;
+    }
+    void set_name(std::string name) {
+        this->name = name;
+    }
+    std::string get_name() {
+        return name;
+    }
+    virtual void print() {
+        std::cout << get_name() << std::endl;
+        std::cout << "Количество сторон: " << get_sides_count() << std::endl;
+        std::cout << std::endl;
+    }
+};
+
+class Triangle : public Figure {
+protected:
+    unsigned int side_a;
+    unsigned int side_b;
+    unsigned int side_c;
+    unsigned short angle_A;
+    unsigned short angle_B;
+    unsigned short angle_C;
+public:
+    Triangle(unsigned int a, unsigned int b, unsigned int c, unsigned short A, unsigned short B, unsigned short C) {
+        this->side_a = a;
+        this->side_b = b;
+        this->side_c = c;
+        this->angle_A = A;
+        this->angle_B = B;
+        this->angle_C = C;
+
+        this->sides_count = 3;
+    }
+    unsigned int get_side_a() {
+        return side_a;
+    }
+    unsigned int get_side_b() {
+        return side_b;
+    }
+    unsigned int get_side_c() {
+        return side_c;
+    }
+    unsigned short get_angle_A() {
+        return angle_A;
+    }
+    unsigned short get_angle_B() {
+        return angle_B;
+    }
+    unsigned short get_angle_C() {
+        return angle_C;
+    }
+    void print() override {
+        std::cout << name << std::endl;
+        std::cout << "Стороны: а=" << get_side_a() << " b=" << get_side_b() << " c=" << get_side_c() << std::endl;
+        std::cout << "Углы: A=" << get_angle_A() << " B=" << get_angle_B() << " C=" << get_angle_C() << std::endl;
+        std::cout << std::endl;
+    }
+};
+
+class RightTriangle : public Triangle {
+public:
+    RightTriangle(unsigned int a, unsigned int b, unsigned int c, unsigned short A, unsigned short B) : Triangle(a, b, c, A, B, 90) {
+
+    }
+};
+
+class EquilateralTriangle : public Triangle {
+public:
+    EquilateralTriangle(unsigned int a) : Triangle(a, a, a, 60, 60, 60) {
+
+    }
+};
+
+class IsoscelesTriangle : public Triangle {
+public:
+    IsoscelesTriangle(unsigned int a, unsigned int b, unsigned short A, unsigned short B) : Triangle(a, b, a, A, B, A) {
+
+    }
+};
+
+class Quadrangle : public Figure {
+public:
+    unsigned int side_a;
+    unsigned int side_b;
+    unsigned int side_c;
+    unsigned int side_d;
+    unsigned short angle_A;
+    unsigned short angle_B;
+    unsigned short angle_C;
+    unsigned short angle_D;
+
+    Quadrangle(unsigned int a, unsigned int b, unsigned int c, unsigned int d, unsigned short A, unsigned short B, unsigned short C, unsigned short D) {
+        this->side_a = a;
+        this->side_b = b;
+        this->side_c = c;
+        this->side_d = d;
+        this->angle_A = A;
+        this->angle_B = B;
+        this->angle_C = C;
+        this->angle_D = D;
+
+        this->sides_count = 4;
+    }
+    unsigned int get_side_a() {
+        return side_a;
+    }
+    unsigned int get_side_b() {
+        return side_b;
+    }
+    unsigned int get_side_c() {
+        return side_c;
+    }
+    unsigned int get_side_d() {
+        return side_d;
+    }
+    unsigned short get_angle_A() {
+        return angle_A;
+    }
+    unsigned short get_angle_B() {
+        return angle_B;
+    }
+    unsigned short get_angle_C() {
+        return angle_C;
+    }
+    unsigned short get_angle_D() {
+        return angle_D;
+    }
+    void print() override {
+        std::cout << name << std::endl;
+        std::cout << "Стороны: а=" << get_side_a() << " b=" << get_side_b() << " c=" << get_side_c() << " d=" << get_side_d() << std::endl;
+        std::cout << "Углы: A=" << get_angle_A() << " B=" << get_angle_B() << " C=" << get_angle_C() << " D=" << get_angle_D() << std::endl;
+        std::cout << std::endl;
+    }
+};
+
+class Parallelogram : public Quadrangle {
+public:
+    Parallelogram(unsigned int a, unsigned int b, unsigned short A, unsigned short B) : Quadrangle(a, b, a, b, A, B, A, B) {
+
+    }
+};
+
+class RectangleC : public Parallelogram {
+public:
+    RectangleC(unsigned int a, unsigned int b) : Parallelogram(a, b, 90, 90) {
+
+    }
+};
+
+class Rhomb : public Parallelogram {
+public:
+    Rhomb(unsigned int a, unsigned short A, unsigned short B) : Parallelogram(a, a, A, B) {
+
+    }
+};
+
+class Square : public Rhomb {
+public:
+    Square(unsigned int a) : Rhomb(a, 90, 90){
+
+    }
+};
+
+void print_info(Figure* figure) {
+    figure->print();
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Figure figure;
+    figure.set_name("Фигура");
+
+    Triangle triangle(10, 20, 30, 45, 45, 90);
+    triangle.set_name("Треугольник");
+
+    RightTriangle right_triangle(4, 6, 7, 35, 55);
+    right_triangle.set_name("Прямоугольный треугольник");
+
+    EquilateralTriangle equilateral_triangle(50);
+    equilateral_triangle.set_name("Равноcторонний треугольник");
+
+    IsoscelesTriangle isosceles_triangle(10, 20, 50, 80);
+    isosceles_triangle.set_name("Равнобедренный треугольник");
+
+    Quadrangle quadrangle(10, 20, 30, 40, 25, 30, 90, 75);
+    quadrangle.set_name("Четырехугольник");
+    
+    Parallelogram parallelogram(15, 30, 120, 60);
+    parallelogram.set_name("Параллелограм");
+
+    RectangleC rectangle(10, 20);
+    rectangle.set_name("Прямоугольник");
+
+    Rhomb rhomb(10, 35, 70);
+    rhomb.set_name("Ромб");
+
+    Square square(5);
+    square.set_name("Квадрат");
+
+    print_info(&figure);
+    print_info(&triangle);
+    print_info(&right_triangle);
+    print_info(&equilateral_triangle);
+    print_info(&isosceles_triangle);
+    print_info(&quadrangle);
+    print_info(&parallelogram);
+    print_info(&rectangle);
+    print_info(&rhomb);
+    print_info(&square);
+}
---
+++ Lesson05/Figures. Sides count/Figures. Sides count.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class Figure {
+protected:
+    int sides_count;
+    std::string name;
+public:
+    Figure(int sides_count) {
+        this->sides_count = sides_count;
+    }
+    int get_sides_count() {
+        return sides_count;
+    }
+    void set_name(std::string name) {
+        this->name = name;
+    }
+    std::string get_name() {
+        return name;
+    }
+};
+
+class Triangle : public Figure {
+public:
+    Triangle(int sides_count) : Figure(sides_count) {
+
+    }
+};
+
+class Quadrangle : public Figure {
+public:
+    Quadrangle(int sides_count) : Figure(sides_count) {
+
+    }
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Figure figure(0);
+    figure.set_name("Фигура");
+
+    Triangle triangle(3);
+    triangle.set_name("Треугольник");
+
+    Quadrangle quadrangle(4);
+    quadrangle.set_name("Четырехугольник");
+
+    std::cout << "Количество сторон: " << std::endl;
+
+    std::cout << figure.get_name() << ": " << figure.get_sides_count() << std::endl;
+    std::cout << triangle.get_name() << ": " << triangle.get_sides_count() << std::endl;
+    std::cout << quadrangle.get_name() << ": " << quadrangle.get_sides_count() << std::endl;
+
+}
