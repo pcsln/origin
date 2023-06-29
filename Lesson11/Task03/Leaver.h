80955d7c67dd09e845624e8f7a9b94d3eb909bfc
pcsln <pcsln@bk.ru>
2023-06-29 10:46:47 +0400

    Fixed Tas03 in Lesson11

--- Lesson11/Task03/Leaver.h
+++ Lesson11/Task03/Leaver.h
@@ -1,12 +1,7 @@
 #pragma once
 #include <iostream>
-#ifdef LEAVERLIBRARYDYNAMIC_EXPORTS
-#define LEAVERLIBRARY_API __declspec(dllexport)
-#else
-#define LEAVERLIBRARY_API __declspec(dllimport)
-#endif
 
 class Leaver {
 public:
-	LEAVERLIBRARY_API std::string leave(std::string str);
+	std::string leave(std::string str);
 };
