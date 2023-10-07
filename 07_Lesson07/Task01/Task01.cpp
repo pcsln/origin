a92abe5a9c76484d1b1603e3afba1f276a2f0314
pcsln <pcsln@bk.ru>
2023-10-07 19:36:01 +0400

    Lesson7 added

---
+++ 07_Lesson07/Task01/Task01.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::vector<int> vector = { 1, 1, 2, 5, 6, 1, 2, 4 };
+    std::cout << "[IN]: ";
+    for (auto it : vector) {
+        std::cout << it << " ";
+    }
+    std::cout << std::endl;
+
+    std::sort(vector.begin(), vector.end());
+
+    auto it = std::unique(vector.begin(), vector.end());
+    vector.erase(it, vector.end());
+
+    std::cout << "[OUT]: ";
+    for (auto it : vector) {
+        std::cout << it << " ";
+    }
+
+}
+
---
+++ 07_Lesson07/Task02/Task02.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+#include <set>
+#include <list>
+#include <iostream>
+
+template<typename T>
+void print_container(const T conteiner) {
+    for (auto it : conteiner) {
+        std::cout << it << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+}
+
+
