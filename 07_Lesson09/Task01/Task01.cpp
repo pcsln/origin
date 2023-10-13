ce3c988524a641207424a5177e5f23be63dab469
pcsln <pcsln@bk.ru>
2023-10-13 23:01:56 +0400

    Lesson9 Task01 added

---
+++ 07_Lesson09/Task01/Task01.cpp
@@ -0,0 +1,20 @@
+﻿#include <vector>
+#include <iostream>
+
+template <typename T>
+void move_vectors(T& lhs, T& rhs) {
+    rhs = std::move(lhs);
+}
+
+
+int main()
+{
+    std::vector <std::string> one = { "test_string1", "test_string2" };
+    std::vector <std::string> two;
+    move_vectors(one, two);
+
+    std::cout << "Vector two" << std::endl;
+    for (auto it : two) {
+        std::cout << it << " ";
+    }
+}
