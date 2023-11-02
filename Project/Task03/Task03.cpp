bba03abb0bcb0ed7eb8bfa7fdf564d1e558238c1
pcsln <pcsln@bk.ru>
2023-11-02 16:37:22 +0400

    Project fixed

--- Project/Task03/Task03.cpp
+++ Project/Task03/Task03.cpp
@@ -10,82 +10,102 @@ private:
     std::fstream file;
     std::string filename;
 public:
+    
     std::map<std::string, std::string> ini_data;
-    ini_parser(std::string filename) {
-        this->filename = filename;
-
-    }
-    ~ini_parser() {}
+    bool is_value_found = true;
+    bool is_file_open = true;
 
     void fill_ini_data() {
         std::string current_section = "";
         std::string string;
-       
+
+        try {
             std::ifstream file(filename);
             if (!file.is_open()) {
-                std::cout << "INI файл не найден или поврежден.";
-                exit(EXIT_FAILURE);
+                throw std::ios_base::failure("Файл не найден или поврежден.");
             }
-
-		while (getline(file, string)) {
-			string.erase(std::remove_if(string.begin(), string.end(), [](char c) { return c == ' '; }), string.end());
-			std::string::size_type n = string.find(';');
-			if (n != std::string::npos) {
-				string.erase(n, string.length() - n);
-			}
-            
-            if (string != "") {
-                if (string[0] == '[') {
-                    current_section = string.substr(1, string.length() - 2);
+        
+            while (getline(file, string)) {
+                string.erase(std::remove_if(string.begin(), string.end(), [](char c) { return c == ' '; }), string.end());
+                std::string::size_type n = string.find(';');
+                if (n != std::string::npos) {
+                    string.erase(n, string.length() - n);
                 }
-                else {
-                    int position = 0;
-                    for (int i = 0; i < string.length(); ++i) {
-                        if (string[i] == '=') {
-                            position = i;
-                            break;
-                        }
+
+                if (string != "") {
+                    if (string[0] == '[') {
+                        current_section = string.substr(1, string.length() - 2);
                     }
-                    ini_data[current_section + "." + string.substr(0, position)] = string.substr(position + 1, string.length() - 1);
+                    else {
+                        int position = 0;
+                        for (int i = 0; i < string.length(); ++i) {
+                            if (string[i] == '=') {
+                                position = i;
+                                break;
+                            }
+                        }
+                        ini_data[current_section + "." + string.substr(0, position)] = string.substr(position + 1, string.length() - 1);
+                    }
                 }
             }
-		}
 
-        file.close();
+            file.close();
 
-        for (auto it : ini_data) {
-            std::cout << it.first << " " << it.second << std::endl;
+            for (auto it : ini_data) {
+                std::cout << it.first << " " << it.second << std::endl;
+            }
+        
         }
-    
+        catch (const std::exception& e) {
+            std::cerr << "Ошибка: " << e.what() << std::endl;
+            file.close();
+            is_file_open = false;
+        }
 
+        
+    }
+
+    ini_parser(std::string filename) {
+        this->filename = filename;
+
+        fill_ini_data();
 
     }
+    ~ini_parser() {}
 
     bool check_value(std::string key) {
         
-        if (ini_data[key] == "") {
-            std::cout << "Значение переменной в файле " << filename << " не определено.";
-            exit(EXIT_FAILURE);
+        try {
+            if (ini_data[key] == "") {
+                throw std::invalid_argument("Значение переменной в файле не определено.");
+            }
+            else {
+                return true;
+            }
         }
-        else {
-            return true;
+        catch (const std::exception& e) {
+            std::cerr << "Ошибка: " << e.what() << std::endl;
+            is_value_found = false;
+            return false;
         }
+     
     }
 
     template <typename T>
     T get_value(std::string key) {
-        fill_ini_data();
+        //fill_ini_data();
         if (check_value(key)) return ini_data[key];
     }
     template <>
     int get_value(std::string key) {
-        fill_ini_data();
+        //fill_ini_data();
         if (check_value(key)) return std::stoi(ini_data[key]);
     }
     template <>
     double get_value(std::string key) {
-        fill_ini_data();
-        if (check_value(key)) return std::atof(ini_data[key].c_str());
+        //fill_ini_data();
+            if (check_value(key)) return std::atof(ini_data[key].c_str()); 
+
     }
 
 };
@@ -96,7 +116,10 @@ int main()
     SetConsoleOutputCP(1251);
     
     ini_parser parser("ini.txt");
-    auto value = parser.get_value<double>("Section1.var1");
+    if (parser.is_file_open) {
+
+        auto value = parser.get_value<double>("Section1.var1");
+        if (parser.is_value_found) std::cout << std::endl << "Значение запрошенного параметра равно " << value << std::endl;
     
-    std::cout << std::endl << "Значение запрошенного параметра равно " << value << std::endl;
+    }
 }
